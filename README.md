7277a0dcfd78081759c7abfd3ae7a2ae48d23130
Maël Brunet <mael.brunet@keytradebank.com>
2022-01-17 11:48:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 20 years bot
+
+A Twitter bot that tells you about things that turn 20.
+# Usage
+
+To test the bot locally, you will need to set environment variables for the following API keys : 'API_KEY', 'API_KEY_SECRET', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET' (from the Twitter API), and 'TMDB_API_KEY' (from [The Movie DB](https://www.themoviedb.org/)).
---
+++ index.js
@@ -0,0 +1,38 @@
+import {} from "dotenv/config";
+import fetch from "node-fetch";
+import { TwitterApi } from "twitter-api-v2";
+
+const client = new TwitterApi({
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_KEY_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+const rwClient = client.readWrite;
+
+const today = new Date();
+const year = today.getFullYear() - 20;
+let month = (today.getMonth() + 1).toString();
+if (month.length < 2) {
+  month = "0" + month;
+}
+let day = (today.getDate()).toString();
+if (day.length < 2) {
+  day = "0" + day;
+}
+
+const tweet = () => {
+    fetch(
+  `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&primary_release_date.gte=${year}-${month}-${day}&primary_release_date.lte=${year}-${month}-${day}&vote_count.gte=200&with_original_language=en`
+)
+  .then((res) => res.json())
+  .then((json) => {
+      json.results.forEach(async (movie) => {
+          rwClient.v2.tweet(`${movie.title} came out 20 years ago`);
+      })
+  })
+  .catch((err) => console.log(err));
+};
+
+tweet();
